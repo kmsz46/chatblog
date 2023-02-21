5bb8f1277d5bc9ac7cfcd0f82cac66de3dcc4033
FujiwaraKotomi <s20t328@stu.kagawa-u.ac.jp>
2023-02-21 17:53:16 +0900

    delete

--- frontend/src/components/Template/test.tsx
+++
@@ -1,9 +0,0 @@
-import TextField from '@mui/material/TextField'
-
-const SubjectForm = () => {
-    return (
-        <TextField id="outlined-basic" label="件名" variant="outlined" sx ={{ display: "flex", maxWidth: 360 }}/>
-    )
-}
-
-export default SubjectForm
