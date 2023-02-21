07071ddb281058472cb10e77a61143426c88f53a
FujiwaraKotomi <s20t328@stu.kagawa-u.ac.jp>
2023-02-21 17:06:28 +0900

    SubjectForm

--- frontend/src/components/Atoms/SubjectForm.tsx
+++ frontend/src/components/Atoms/SubjectForm.tsx
@@ -1,5 +1,9 @@
-const SubjectForm = () => {
+import TextField from '@mui/material/TextField'
 
+const SubjectForm = () => {
+    return (
+        <TextField id="outlined-basic" label="件名" variant="outlined" sx ={{ display: "flex", maxWidth: 360 }}/>
+    )
 }
 
 export default SubjectForm
---
+++ frontend/src/components/Template/button.tsx
@@ -0,0 +1,10 @@
+import Button from '@mui/material/Button'
+
+const Buttonform = () => {
+    return(
+        <Button variant = "outlined" color = "error">ボタンの名前</Button>
+        // colorがsuccess(緑)、error(赤)
+    )
+}
+
+export default Buttonform
