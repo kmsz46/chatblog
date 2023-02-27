ac2d0f897dcc22631f1b8f9d4d8ae22b2bdfa995
ganbon <s20t301@stu.kagawa-u.ac.jp>
2023-02-27 15:33:30 +0900

    不要なファイル削除

--- frontend/src/components/Organisms/ThreadFind.tsx
+++
@@ -1,8 +0,0 @@
-import Axios from 'axios'
-
-async function ThreadFind (title:string) {
-    const response = await Axios.post("http://127.0.0.1:5000/contents",{title:title})
-    return response
-}
-
-export default ThreadFind
--- frontend/src/components/Organisms/TitleGet.tsx
+++
@@ -1,9 +0,0 @@
-import Axios from 'axios'
-
-
-async function TitleGet () {
-    const response = await Axios.post("http://127.0.0.1:5000/display")
-    return response
-  }
-
-export default TitleGet
