02db978f13cbe4c6fe5129ddea84aacf3870e1af
neko-20 <zkaiyou03@gmail.com>
2023-03-10 11:19:04 +0900

    アイコンボタン用型定義修正版

--- frontend/src/types/IconButtonProps.tsx
+++ frontend/src/types/IconButtonProps.tsx
@@ -1,9 +1,12 @@
-import {ReactNode} from 'react';
+import { ReactNode } from 'react';
+import { SxProps, Theme } from '@mui/material/styles';
 
 type IconButtonPropsType = {
-  icon: ReactNode;
+  icon?: ReactNode;
+  children?: ReactNode;
   color?: "inherit" | "primary" | "secondary" | "success" | "error" | "info" | "warning";
   size?: "small" | "medium" | "large";
+  sx?: SxProps<Theme>;
   handleClick?: () => void;
 }
 export default IconButtonPropsType;
