01e5bd6f9371b0b2994e62d7fe93e36b81e5e31b
FujiwaraKotomi <s20t328@stu.kagawa-u.ac.jp>
2023-03-23 18:30:47 +0900

    ボタンを修正し、ページ遷移するようにしました。

--- frontend/src/components/Molecules/Header.tsx
+++ frontend/src/components/Molecules/Header.tsx
@@ -1,24 +1,27 @@
-import AccountButton from "../Atoms/AccountButton";
-import MakeThreadButton from "../Atoms/MakeThreadButton";
 import AppBar from '@mui/material/AppBar'
 import Toolbar from "@mui/material/Toolbar";
 import Box from '@mui/material/Box';
-
+import BorderColorIcon from '@mui/icons-material/BorderColor';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import MultipleButton from "../Atoms/MultipleButton";
+import MultipleIconButton from "../Atoms/MultipleIconButton";
 
 const Header = () => {
-    return(
-        <div style = {{ width:"100%" }}>
-       <AppBar>
-            <Toolbar>
-                <div style={{ flexGrow: 1 }}></div>
-                <Box>
-                    <MakeThreadButton/> 
-                    <AccountButton />
-                </Box>
-            </Toolbar>
-       </AppBar>
-       </div>
-    )
+  return(
+    <div style={{ width:"100%" }}>
+      <AppBar position="sticky">
+        <Toolbar>
+          <div style={{ flexGrow: 1 }}></div>
+          <Box>
+            <MultipleButton text="作成" variant="outlined" color="inherit" startIcon={<BorderColorIcon />} 
+              handleClick={() => window.location.href='/threadwrite'}/>
+            <MultipleIconButton icon={<AccountCircleIcon/>} size="large" color="inherit"
+              handleClick={() => window.location.href='/id'}/>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
 }
 
 export default Header
