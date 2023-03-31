8f93e36eba661da822c63980215471df9e191d62
neko-20 <zkaiyou03@gmail.com>
2023-03-31 14:18:04 +0900

    SelectBoxからAutocompleteに変更

--- frontend/src/components/Molecules/SettingsModal.tsx
+++ frontend/src/components/Molecules/SettingsModal.tsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
-import MultipleSelectBox from '../Atoms/MultipleSelectBox'
 import MultipleButton from '../Atoms/MultipleButton';
 import MultipleIconButton from '../Atoms/MultipleIconButton';
 import CloseIcon from "@mui/icons-material/Close";
+import InputTags from '../Atoms/InputTags';
+import InputTag from '../Atoms/InputTag';
 
 // modalのスタイル
 const styles = {
@@ -22,7 +23,7 @@ const styles = {
 
 // 表示設定用モーダルコンポ―ネント
 const SettingsModal = () => {
-  const [selectTag, setSelectTag] = React.useState<string[]>([]);
+  const [selectTag, setSelectTag] = React.useState<string>('');
   const [selectGroup, setSelectGroup] = React.useState<string[]>([]);
   const [open, setOpen] = React.useState(false);
  
@@ -46,15 +47,11 @@ const SettingsModal = () => {
     setOpen(false);
   };
 
-  // 仮のタグリスト
-  const tagList = [
-    'c',
-    'c++',
-    'java',
-    'python',
-    'go',
-    'vue',
-    'react',
+  const maintagList = [
+    '質問',
+    '提案',
+    'お願い',
+    'お知らせ'
   ];
 
   // 仮のグループリスト
@@ -85,7 +82,7 @@ const SettingsModal = () => {
         <Box sx={styles}>
           <Box sx = {{ borderBottom:1, display:"flex", justifyContent:"space-around"}}>
             {/* タイトル */}
-            <Typography id="modal-modal-title" variant="h6" component="h2">
+            <Typography variant="h6" component="h2">
               表示設定
             </Typography>
 
@@ -94,13 +91,13 @@ const SettingsModal = () => {
             <MultipleIconButton icon={<CloseIcon />} sx = {{ marginBottom:1 }} handleClick={() => setOpen(false)}/>
           </Box>
 
-          {/* タグのセレクトボックス */}
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>タグ</Typography>
-          <MultipleSelectBox options={tagList} selected={selectTag} setSelected={setSelectTag} />                         
+          {/* メインタグ */}
+          <Typography sx={{ mt: 2 }}>タグ</Typography>
+          <InputTag options={maintagList} tag={selectTag} setTag={setSelectTag} placeholder={"メインタグ選択"} sx={{ m: 1, width: 470 }}/>                         
           
-          {/* 所属グループのセレクトボックス */}
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>グループ</Typography>
-          <MultipleSelectBox options={groupList} selected={selectGroup} setSelected={setSelectGroup} />
+          {/* 所属グループ */}
+          <Typography sx={{ mt: 2 }}>グループ</Typography>
+          <InputTags options={groupList} tags={selectGroup} setTags={setSelectGroup} placeholder={"グループ選択"} sx={{ m: 1, width: 470 }}/>  
           
           {/* 決定ボタン */}
           <Box sx = {{marginTop:2, display: "flex", justifyContent: "space-around" }}>
